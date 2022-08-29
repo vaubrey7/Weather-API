10216f9442c0f211cc09106044d1f5393ab36f86
Vaughn Aubrey <vaubrey7@gmail.com>
2022-08-29 12:17:42 -0600

    five day

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -39,31 +39,31 @@ let weathersharknado = {
       });
   }, // adding elements and appending directly to the HTML for rest of the forecast. 
   displayFiveDay: function (data, city) {
-    const fiveDayForecast = documnet.querySelector("#fiveDayForecast")
-    for (var i = 0; i < data.length; i += 8) {
+    console.log(city,data )
+    const fiveDayForecast = document.querySelector("#fiveDayForecast")
+    for (var i = 0; i < data.list.length; i += 8) {
       const h1El = document.createElement("h1")
        h1El.classList.add("weather-cityboy")
         h1El.textContent = city
       const h2El = document.createElement("h2")
        h2El.classList.add("weather-temp")
-        h2El.document.querySelector(".weather-temp").innerText = Math.floor(temp) + "°F";
-         document.querySelector(".weather-humidity").innerText =
-           "Humidity: " + humidity + "%";
-      const divEl = document.createElement("div")
+       h2El.textContent = Math.floor(data.list[i].main.temp) + "°F";
+       fiveDayForecast.append(h1El, h2El)
+        const divEl = document.createElement("div")
        divEl.classList.add("flex")
-      const imgEl = document.createElement(`img src="https://openweathermap.org/img/wn/04n.png" alt=""`)
-        imgEl.classList.add("westher-icon")
-         imgEl.append("appending to divEl")
+      const imgEl = document.createElement(`img`)
+      imgEl.src = "https://openweathermap.org/img/wn/04n.png" 
+        imgEl.classList.add("weather-icon")
           divEl.append(imgEl)
       const weatherDescEL = document.createElement("div")
        weatherDescEL.classList.add("weather-description")
-         weatherDescEL.append("appending to the div")
+       weatherDescEL.textContent = (data.list[i].weather[i].description) 
           divEl.append(weatherDescEL)
-      const weatherHumidEl = document.createElement("div")
-       weatherHumidEl.classList.add("weather-humidity")
       const weatherWind = document.createElement("div")
        weatherWind.classList.add("weather-wind")
-       this.displayFiveDay.$(content).appendTo(HTML);
+       weatherWind.textContent = (data.list[i].wind.speed) + " mp/h";
+       divEl.append(weatherWind)
+       fiveDayForecast.append(divEl);
     }
   },
 
@@ -121,107 +121,3 @@ document
 //Sets default weather location
 weathersharknado.fetchWeather("Salt Lake City");
 
-//=============================================================================================================     
-
-//INTERATION 2 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
-//weather.fetchWeather("").city.name
-// if (filteredArray.length > 0) {
-//   msg.textContent = `You already know the weather for ${
-//     filteredArray[0].querySelector(".city-name span").textContent
-//   }`
-//   form.reset();
-//   input.focus();
-//   return;
-// }
-// }
-
-// let weather = {
-//   apiKey: "2e8b44f9196feb70c52b445f0e1cca3a",
-//   fetchWeather: function (city) {
-//     fetch(
-//       "https://api.openweathermap.org/data/2.5/weather?q=" +
-//         city +
-//         "&units=imperial&appid=" +
-//         this.apiKey
-//     )
-//       .then((response) => {
-//         if (!response.ok) {
-//           alert("No weather found.");
-//           throw new Error("No weather found.");
-//         }
-//         return response.json();
-//       })
-//       .then((data) => this.displayWeather(data));
-
-//       for (var i = 0; i < list.length; i + 8) {
-//         console.log(i)
-//       }
-
-//   },
-//   displayWeather: function (data) {
-//     const { name } = data;
-//     const { icon, description } = data.weather[0];
-//     const { temp, humidity } = data.main;
-//     const { speed } = data.wind;
-//     document.querySelector(".city").innerText = "Weather in " + name;
-//     document.querySelector(".icon").src =
-//       "https://openweathermap.org/img/wn/" + icon + ".png";
-//     document.querySelector(".description").innerText = description;
-//     document.querySelector(".temp").innerText = temp + "°F";
-//     document.querySelector(".humidity").innerText =
-//       "Humidity: " + humidity + "%";
-//     document.querySelector(".wind").innerText =
-//       "Wind speed: " + speed + " mp/h";
-//     document.querySelector(".weather").classList.remove("loading");
-//     document.body.style.backgroundImage =
-//       "url('https://source.unsplash.com/1600x900/?" + name + "')";
-//   },
-//   }; 
-//   document.querySelector("search-by-city")
-//   .addEventListener("keyup", function (event) {
-//     if (event.key == "Enter") {
-//       weather.search();
-// }})
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-//INTERATION 1 ---------------------------------------------------------------------------------------------------------------------------------
-// const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${apiKey}&units=imperial`;
-// fetch(url.data.list.weather).then(response => response.JSON())
-// .then(data => {
-//   const {
-//     main,
-//     name,
-//     sys,
-//     list,
-//     weather,
-//   } = data;
-// for (var i = 0; i < list.length; i + 8) {
-//   console.log(i)
-// }
-//     document.querySelector("search-by-city")
-// .addEventListener("keyup", function (event) {
-//   if (event.key == "Enter") {
-//     weather.search();
-//   }
-// });
-
-//   const li = document.createElement("li");
-//   li.classList.add("city");
-//   const markup = `
-//     <h2 class="city-name" data-name="${name},${sys.country}">
-//       <span>${name}</span>
-//       <sup>${sys.country}</sup>
-//     </h2>S
-//     <div class="city-temp">${Math.round(main.temp)}<sup>°F</sup></div>`
-
-// })
-
-// .catch(() => {
-//   msg.textContent = "Please search for a valid city";
-// });
-
-// msg.textContent = "";
-// form.reset();
-// input.focus();
-//----------------------------------------------------------------------------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Weather-API
-had to delete my repo several times because it pulled my whole desktop into the repo. 
-    API key 2bc8d7fa0b6f7260daae03b9644810969f321540
+I had to delete my repo several times because it pulled my whole desktop into the repo. 
+    API key 2bc8d7fa0b6f7260daae03b9644810969f321540 its a Free API
 ![2022-08-25 (2)](https://user-images.githubusercontent.com/103152585/186754756-3e6bc6cd-225d-4f7c-ab21-0a48ede057e7.png)
 ![2022-08-25 (3)](https://user-images.githubusercontent.com/103152585/186754781-7e4e4b8b-fe37-4b8a-a1e0-8cf932f70fa6.png)
 ![2022-08-25 (4)](https://user-images.githubusercontent.com/103152585/186754793-7b95bf8f-aa5a-4252-87e0-86ce2497a3fe.png)
